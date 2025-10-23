f482a1b61b80f002b0a1d62c325968515d87e00e
DanieleSavino <daniele.savino0@gmail.com>
2025-10-23 20:11:07 +0200

    Added Flutter lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
       require("mason").setup()
     end,
   },
-
   -- Mason-LSP integration
   {
     "mason-org/mason-lspconfig.nvim",
@@ -18,79 +17,117 @@ return {
       })
     end,
   },
-
-  -- Completion engine first!
+  -- Mason tool installer for formatters/linters
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          -- Dart/Flutter
+          "dart-debug-adapter",
+        },
+      })
+    end,
+  },
+  -- Flutter Tools (includes LSP, formatting, and more)
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- LSP source
-      "L3MON4D3/LuaSnip", -- snippet engine
-      "saadparwaiz1/cmp_luasnip",
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
     },
     config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- Get capabilities from blink.cmp instead of nvim-cmp
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-      -- LSP setup after cmp is loaded
-      local lspconfig = require("lspconfig")
-      local on_attach = function(client, bufnr)
-        local buf_map = function(mode, lhs, rhs, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, lhs, rhs, opts)
-        end
-        buf_map("n", "gd", vim.lsp.buf.definition)
-        buf_map("n", "gr", vim.lsp.buf.references)
-        buf_map("n", "K", vim.lsp.buf.hover)
-        buf_map("n", "<leader>rn", vim.lsp.buf.rename)
-      end
-
-      local servers = { "pyright", "lua_ls", "clangd", "jdtls" }
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end
-
-      -- CMP setup
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+      require("flutter-tools").setup({
+        ui = {
+          border = "rounded",
+          notification_style = "native",
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
+        decorations = {
+          statusline = {
+            app_version = true,
+            device = true,
+          },
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+        },
+        widget_guides = {
+          enabled = true,
+        },
+        closing_tags = {
+          highlight = "Comment",
+          prefix = "// ",
+          enabled = true,
+        },
+        dev_log = {
+          enabled = true,
+          open_cmd = "tabedit",
+        },
+        lsp = {
+          color = {
+            enabled = true,
+            background = false,
+            foreground = false,
+            virtual_text = true,
+            virtual_text_str = "■",
+          },
+          on_attach = function(client, bufnr)
+            local buf_map = function(mode, lhs, rhs, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, lhs, rhs, opts)
             end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }),
+            -- LSP keymaps
+            buf_map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            buf_map("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+            buf_map("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+            buf_map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+            buf_map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+            -- Flutter specific keymaps
+            buf_map("n", "<leader>fc", "<cmd>Telescope flutter commands<CR>", { desc = "Flutter commands" })
+            buf_map("n", "<leader>fr", "<cmd>FlutterRun<CR>", { desc = "Flutter run" })
+            buf_map("n", "<leader>fq", "<cmd>FlutterQuit<CR>", { desc = "Flutter quit" })
+            buf_map("n", "<leader>fd", "<cmd>FlutterDevices<CR>", { desc = "Flutter devices" })
+            buf_map("n", "<leader>fe", "<cmd>FlutterEmulators<CR>", { desc = "Flutter emulators" })
+            buf_map("n", "<leader>fR", "<cmd>FlutterReload<CR>", { desc = "Flutter reload" })
+            buf_map("n", "<leader>fs", "<cmd>FlutterRestart<CR>", { desc = "Flutter restart" })
+            buf_map("n", "<leader>fo", "<cmd>FlutterOutlineToggle<CR>", { desc = "Flutter outline" })
+            buf_map("n", "<leader>fD", "<cmd>FlutterDevTools<CR>", { desc = "Flutter DevTools" })
+          end,
+          capabilities = capabilities,
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+            renameFilesWithClasses = "prompt",
+            enableSnippets = true,
+            updateImportsOnRename = true,
+          },
+        },
       })
-
-      -- LuaSnip loader
-      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.dart = { "dart_format" }
+      return opts
+    end,
+  },
+  -- Linting
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      -- Dart analysis is handled by flutter-tools LSP
+      return opts
     end,
   },
 }
